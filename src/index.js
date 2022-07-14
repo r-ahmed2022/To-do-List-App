61a877672ac5ba3797943ca7ecec0a201b8804f4
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-15 05:12:26 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import refresh from './refresh.png';
 
 const tasks = [
   {
-    index: 2,
+    index: 3,
     description: 'to finish to do list app',
     completed: false,
 
   },
 
   {
-    index: 1,
+    index: 2,
     description: 'to finish daily tasks',
     completed: true,
 
   },
 
   {
-    index: 3,
+    index: 1,
     description: 'to finish standup meeting',
     completed: true,
 
